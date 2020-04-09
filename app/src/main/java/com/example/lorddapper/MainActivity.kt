4ff6b1aea16423b2401ac141370330527632a950
Fergus <fme36@uclive.ac.nz>
2020-04-09 16:31:55 +1200

    remade project

--- app/src/main/java/com/example/lorddapper/MainActivity.kt
+++ app/src/main/java/com/example/lorddapper/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.lorddapper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,9 +9,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    /**Called when the users presses the 'Go' button */
-    fun searchDesigns(view: View) {
-        val intent = Intent(this, SearchResultsActivity::class.java)
-    })
 }
