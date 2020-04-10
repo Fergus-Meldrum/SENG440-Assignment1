389b38fddcf28b692836d1d7e7a9db53cc28444f
Fergus <fme36@uclive.ac.nz>
2020-04-10 20:59:29 +1200

    added 5 activities. added use of camera app.

--- app/src/main/java/com/example/lorddapper/MainActivity.kt
+++ app/src/main/java/com/example/lorddapper/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.lorddapper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sellButton: Button = findViewById(R.id.sellButton)
+
+        sellButton.setOnClickListener {
+            val intent = Intent(this, Sell_Item1::class.java)
+            startActivity(intent)
+        }
+
+        val goButton: Button = findViewById(R.id.goButton)
+
+        goButton.setOnClickListener {
+            val intent = Intent(this, BrowseItems::class.java)
+            startActivity(intent)
+        }
     }
 }
